272d5fd54efb7073e9e7a8b61f3221510117bafd
Mike_Ford <fordmich@mail.gvsu.edu>
2018-06-16 21:10:07 -0400

    Adding program state globals

--- Globals.h
+++ Globals.h
@@ -1,7 +1,11 @@
 #ifndef _Globals_h
 #define _Globals_h
 
-//Define I/O pins for each color
+//Program States
+#define MAIN_SCREEN 1
+#define DETECTION_SCREEN 2
+
+//LED Pins
 #define RED_LED_PIN 5
 #define GREEN_LED_PIN 6
 
